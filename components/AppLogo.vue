2b202ec49add173b57b4355601df90bd26ea7d80
Hyeonjong <nianelo4@gmail.com>
2018-07-11 00:52:29 +0900

    Change whitespace from 2 spaces to tab

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,79 +1,79 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"/>
-    <div class="Triangle Triangle--one"/>
-    <div class="Triangle Triangle--three"/>
-    <div class="Triangle Triangle--four"/>
-  </div>
+	<div class="VueToNuxtLogo">
+		<div class="Triangle Triangle--two"/>
+		<div class="Triangle Triangle--one"/>
+		<div class="Triangle Triangle--three"/>
+		<div class="Triangle Triangle--four"/>
+	</div>
 </template>
 
 <style>
 .VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 72px;
-  width: 98px;
+	display: inline-block;
+	animation: turn 2s linear forwards 1s;
+	transform: rotateX(180deg);
+	position: relative;
+	overflow: hidden;
+	height: 72px;
+	width: 98px;
 }
 
 .Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 0;
+	height: 0;
 }
 
 .Triangle--one {
-  border-left: 42px solid transparent;
-  border-right: 42px solid transparent;
-  border-bottom: 72px solid #41B883;
+	border-left: 42px solid transparent;
+	border-right: 42px solid transparent;
+	border-bottom: 72px solid #41B883;
 }
 
 .Triangle--two {
-  top: 12px;
-  left: 14px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #3B8070;
+	top: 12px;
+	left: 14px;
+	animation: goright 0.5s linear forwards 3.5s;
+	border-left: 35px solid transparent;
+	border-right: 35px solid transparent;
+	border-bottom: 60px solid #3B8070;
 }
 
 .Triangle--three {
-  top: 24px;
-  left: 14px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 28px solid transparent;
-  border-right: 28px solid transparent;
-  border-bottom: 48px solid #35495E;
+	top: 24px;
+	left: 14px;
+	animation: goright 0.5s linear forwards 3.5s;
+	border-left: 28px solid transparent;
+	border-right: 28px solid transparent;
+	border-bottom: 48px solid #35495E;
 }
 
 .Triangle--four {
-  top: 48px;
-  left: 28px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 14px solid transparent;
-  border-right: 14px solid transparent;
-  border-bottom: 24px solid #fff;
+	top: 48px;
+	left: 28px;
+	animation: godown 0.5s linear forwards 3s;
+	border-left: 14px solid transparent;
+	border-right: 14px solid transparent;
+	border-bottom: 24px solid #fff;
 }
 
 @keyframes turn {
-  100% {
-    transform: rotateX(0deg);
-  }
+	100% {
+		transform: rotateX(0deg);
+	}
 }
 
 @keyframes godown {
-  100% {
-    top: 72px;
-  }
+	100% {
+		top: 72px;
+	}
 }
 
 @keyframes goright {
-  100% {
-    left: 28px;
-  }
+	100% {
+		left: 28px;
+	}
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,50 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-study
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-    </div>
-  </section>
+	<section class="container">
+		<div>
+			<app-logo/>
+			<h1 class="title">
+				nuxt-study
+			</h1>
+			<h2 class="subtitle">
+				Nuxt.js project
+			</h2>
+		</div>
+	</section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
-  }
+	components: {
+		AppLogo
+	}
 }
 </script>
 
 <style>
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+	font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+	display: block;
+	font-weight: 300;
+	font-size: 100px;
+	color: #35495e;
+	letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+	font-weight: 300;
+	font-size: 42px;
+	color: #526488;
+	word-spacing: 5px;
+	padding-bottom: 15px;
 }
 </style>
