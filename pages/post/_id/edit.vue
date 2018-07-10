673098d26829ca947d1ca6be762db568fef9112e
Hyeonjong <nianelo4@gmail.com>
2018-07-11 01:28:40 +0900

    Add page route like django blog

---
+++ pages/post/_id/edit.vue
@@ -0,0 +1,15 @@
+<template>
+	<section>
+		<div>
+			<p>Modify existing post</p>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	validate({ params }) {
+		return /^\d+$/.test(params.id)
+	}
+}
+</script>
---
+++ pages/post/new.vue
@@ -0,0 +1,7 @@
+<template>
+	<section>
+		<div>
+			<p>Write new post</p>
+		</div>
+	</section>
+</template>
