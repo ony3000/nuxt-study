5df5b3318a85dfffacbf010b2c557d4111f1abc9
Hyeonjong <nianelo4@gmail.com>
2018-07-12 00:32:24 +0900

    Move page header from index page to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,24 @@
 <template>
 	<div>
-		<nuxt/>
+		<div class="page-header">
+			<app-logo/>
+			<h1 class="title">nuxt-study</h1>
+		</div>
+		<div class="content container">
+			<nuxt/>
+		</div>
 	</div>
 </template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+	components: {
+		AppLogo
+	}
+}
+</script>
 
 <style>
 html {
@@ -19,5 +35,14 @@ html {
 *, *:before, *:after {
 	box-sizing: border-box;
 	margin: 0;
+}
+
+.title {
+	font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+	display: inline;
+	font-weight: 300;
+	font-size: 40px;
+	color: #35495e;
+	letter-spacing: 1px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,7 @@
 <template>
-	<section class="container">
+	<section>
 		<div>
-			<app-logo/>
-			<h1 class="title">
-				nuxt-study
-			</h1>
+			<p>View all posts</p>
 		</div>
 	</section>
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-	components: {
-		AppLogo
-	}
-}
-</script>
-
-<style>
-.container {
-	min-height: 100vh;
-}
-
-.title {
-	font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-	display: inline;
-	font-weight: 300;
-	font-size: 40px;
-	color: #35495e;
-	letter-spacing: 1px;
-}
-</style>
